0f4ec446e3e961cba1502d9243627741522a24d3
xARIZONAx <panmietek000@gmail.com>
2026-07-06 21:08:38 +0200

    Add Supabase public config

---
+++ js/supabase-config.js
@@ -0,0 +1,4 @@
+window.BEAUTY_SUPABASE = {
+  url: 'https://jkeacizrgnzzljtogsqt.supabase.co',
+  anonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImprZWFjaXpyZ256emxqdG9nc3F0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODMzNTY3OTMsImV4cCI6MjA5ODkzMjc5M30.hnz6QuqEHfJZxMKh1x3txwyenIa4t9a7eL0U6IZSaIA'
+};
